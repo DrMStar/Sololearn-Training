c1f62c31a28aea1a6a00b9425b0216d5680a98ab
DrMStar <122894782+DrMStar@users.noreply.github.com>
2023-02-25 14:18:43 +0100

    Create Guard_Flamingos.py

---
+++ Guard_Flamingos.py
@@ -0,0 +1,27 @@
+"""
+Guard Flamingos
+You decide to add a pink flamingo lawn ornament every 2 feet around the perimeter of your yard. How many flamingos do you need to purchase?
+
+Task: 
+Given the length and width of your rectangular yard, determine how many flamingos your should buy to put one every 2 feet along the edges of your yard.
+
+Input Format: 
+Two integer values that represent the length and width of your front yard.
+
+Output Format: 
+An integer that represents the total number of flamingos that you should purchase.
+
+Sample Input: 
+10
+10
+
+Sample Output: 
+20
+"""
+width = int(input())
+length = int(input())
+
+per = 2 * width + 2 * length
+count = per // 2
+
+print(count)
